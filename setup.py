de341105670a849dd48d0f3c75567594b7b5c1bb
Etienne Detoisien <detoisienetienne@gmail.com>
2024-01-30 01:37:58 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-pagerouter',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        # Ajoutez d'autres dépendances ici
+    ],
+    author='Etienne DTS',
+    description='A lightweight Python library for page routing in Flask applications.',
+    license='MIT',
+)
